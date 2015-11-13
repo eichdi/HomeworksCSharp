f8de5610fab52730764eabd6e1425b2c07575691
eichdi <eichdi96@gmail.com>
2015-11-14 01:08:27 +0300

    Fix bag

--- MaxLenghtSubset/MaxLenghtSubset/Program.cs
+++ MaxLenghtSubset/MaxLenghtSubset/Program.cs
@@ -1,43 +1,38 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.IO;
 
-namespace MaxLenghtSubset
-{
-    class Program
+
+    public class MaxLenghtSubset
     {
-        static void Main(string[] args)
+        public static void Main()
         {
             StreamReader a = new StreamReader("input.txt");
             StreamWriter b = new StreamWriter("output.txt");
             int lenght = int.Parse(a.ReadLine());
-            string[] num= a.ReadLine().Split();
-            int result=0;
-            int max=0;
-            int pred=int.MinValue;
+            string[] num = a.ReadLine().Split();
+            int result = 0;
+            int max = 0;
+            int pred = int.MinValue;
             for (int i = 0; i < lenght; i++)
             {
-                if (pred < int.Parse(num[i]))
+                if (pred <= int.Parse(num[i]))
                 {
                     max++;
                 }
                 else
                 {
-                    if (max > result)
-                    {
-                        result = max;
-                    }
-                    max=0;
+                    max = 1;
+                }
+                if (max > result)
+                {
+                    result = max;
                 }
                 pred = int.Parse(num[i]);
             }
 
-            b.Write(result);
-          
-
+            b.WriteLine(result);
+            a.Close();
+            b.Close();
         }
     }
-}
+
